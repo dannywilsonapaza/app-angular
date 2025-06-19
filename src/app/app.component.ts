87e080d8190e4f80da210f62816b635da2869ccd
Danny <dannyapaza40@gmail.com>
2025-06-19 15:37:24 -0500

    pipes en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { MensajeService } from './mensaje.service';
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
 import { ComponenteEnLineaComponent } from "./componente-en-linea/componente-en-linea.component";
@@ -13,16 +13,22 @@ import { AgregarTareaComponent } from "./agregar-tarea/agregar-tarea.component";
 import { ComponenteForComponent } from "./componente-for/componente-for.component";
 import { ViewChildComponent } from "./view-child/view-child.component";
 import { ListadoUsuariosComponent } from "./listado-usuarios/listado-usuarios.component";
+import { EjemploPipesComponent } from "./ejemplo-pipes/ejemplo-pipes.component";
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
+//Registramos los datos de localización para español
+registerLocaleData(localeEs,'es')
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteEnLineaComponent, InterpolacionComponent, PadreComponent, MostrarMensajeComponent, ReplicadorComponent, SaludarComponent, ComponenteIfComponent, AgregarTareaComponent, ComponenteForComponent, ViewChildComponent, ListadoUsuariosComponent],
+  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteEnLineaComponent, InterpolacionComponent, PadreComponent, MostrarMensajeComponent, ReplicadorComponent, SaludarComponent, ComponenteIfComponent, AgregarTareaComponent, ComponenteForComponent, ViewChildComponent, ListadoUsuariosComponent, EjemploPipesComponent],
+  providers: [{provide:LOCALE_ID,useValue: 'es'}],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  titulo = 'Servicios y Observables en Angular';
+  titulo = 'Ejemplo Pipes en Angular';
 
   mensaje: string;
 
